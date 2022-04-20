a34b355fad83de9b91bfc6c22df2323c2cc584b1
Mischka Booley <mischka@gearbox.com.au>
2022-04-20 22:01:09 +1000

    commit tree 414d2415bdaae684a5b03751037a79c384383a9b

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increment(n) {
+  let number = n;
+  number++;
+  return number;
+}
+
+function decrement(n) {
+  let number = n;
+  number--;
+  return number;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
